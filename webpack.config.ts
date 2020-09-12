5948ac1039cb0f7c2fc431e158f1e93947dc0658
Nils <nils@epita.fr>
2020-09-12 18:14:56 +0200

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,60 @@
+import path from 'path';
+import webpack from 'webpack';
+import {} from 'webpack-dev-server';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+
+const {
+  NODE_ENV = 'development',
+  HOST = '0.0.0.0',
+  PORT = '8000',
+} = process.env;
+
+const dev = NODE_ENV === 'development';
+const prod = NODE_ENV === 'production';
+
+const config: webpack.Configuration = {
+
+  mode: prod ? 'production' : 'development',
+  devtool: dev ? 'source-map' : false,
+
+  entry: './src/index.tsx',
+
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            cacheDirectory: true,
+          },
+        },
+      },
+    ],
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin(),
+    dev && new ReactRefreshWebpackPlugin(),
+  ].filter(Boolean),
+
+  devServer: {
+    host: HOST,
+    port: Number(PORT),
+    hot: true,
+  },
+
+};
+
+export default config;
